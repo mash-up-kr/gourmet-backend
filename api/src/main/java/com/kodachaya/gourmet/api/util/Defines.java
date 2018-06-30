f70e838021cc58b980b016cc5f7f24115150bd84
bigstark <kkjkkdk@gmail.com>
2018-07-01 04:48:25 +0900

    Github Issue #7 edit Me Controller

---
+++ api/src/main/java/com/kodachaya/gourmet/api/util/Defines.java
@@ -0,0 +1,9 @@
+package com.kodachaya.gourmet.api.util;
+
+public class Defines {
+
+    public static final String HEADER_AUTHORIZATION = "Authorization";
+
+    public static final String PREFIX_BEARER = "Bearer ";
+
+}
