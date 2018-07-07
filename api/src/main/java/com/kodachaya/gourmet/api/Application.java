09713000ecbd3f1df606718fdb2cf02aacb1e848
bigstark <kkjkkdk@gmail.com>
2018-07-07 12:05:43 +0900

    JPA setting

--- api/src/main/java/com/kodachaya/gourmet/api/Application.java
+++ api/src/main/java/com/kodachaya/gourmet/api/Application.java
@@ -2,8 +2,11 @@ package com.kodachaya.gourmet.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = { Jsr310JpaConverters.class }, basePackages = {"com.kodachaya.gourmet.api.entity"})
 public class Application {
 
 
