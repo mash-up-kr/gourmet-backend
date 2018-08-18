3441146c03619f839be1eab2c083286b16728176
jine9529 <dlswlgp9529@naver.com>
2018-08-18 17:45:01 +0900

    stamp list dummy data

--- api/src/main/java/com/kodachaya/gourmet/api/controller/me/MeController.java
+++ api/src/main/java/com/kodachaya/gourmet/api/controller/me/MeController.java
@@ -1,8 +1,12 @@
 package com.kodachaya.gourmet.api.controller.me;
 
 import com.kodachaya.gourmet.api.dto.BaseListModel;
+import com.kodachaya.gourmet.api.dto.Stamp;
 import com.kodachaya.gourmet.api.dto.me.MeCommand;
+import com.kodachaya.gourmet.api.dto.menu.MenuModel;
+import com.kodachaya.gourmet.api.dto.restaurant.RestaurantModel;
 import com.kodachaya.gourmet.api.dto.review.ReviewModel;
+import com.kodachaya.gourmet.api.dto.review.Taste;
 import com.kodachaya.gourmet.api.dto.user.UserModel;
 import com.kodachaya.gourmet.api.entity.UserEntity;
 import com.kodachaya.gourmet.api.exception.BadRequestException;
@@ -16,6 +20,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.rmi.registry.Registry;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -78,12 +86,48 @@ public class MeController {
         return user;
     }
 
+
     // swagger setting
     @RequestMapping(value = "/me/stamps", method = RequestMethod.GET)
     public @ResponseBody BaseListModel<ReviewModel> getStamps(
             @RequestParam("count") Optional<Integer> optionalCount,
             @RequestParam("cursor") Optional<Integer> cursor) {
+
         // return dummy data
+                UserModel user = new UserModel();
+        Stamp stamp = Stamp.DISLIKE;
+        RestaurantModel restaurant = new RestaurantModel(1, "jine,", 128, 111, LocalDateTime.now());
+        MenuModel menu = new MenuModel(2, "chocolate", 10000, true, null, LocalDateTime.now());
+        List<Taste> taste = new ArrayList<>();
+
+
+        user.setId(2);
+        user.setUsername("testUser");
+        user.setIntroduce("이수언니 초콜릿 먹고싶음");
+        user.setProfileImage("https://s3.ap-northeast-2.amazonaws.com/mf-fileserver/Images/PROFILE/1804170044479124.jpg");
+        user.setStampCount(100);
+        user.setWishCount(1000);
+        user.setFollowingCount(300);
+        user.setFollowerCount(100);
+        user.setIsPublic(true);
+
+        taste.add(Taste.SWEET);
+
+
+        ReviewModel r1 = new ReviewModel(1,user,stamp,restaurant,menu,taste);
+        ReviewModel r2 = new ReviewModel(2,user,stamp,restaurant,menu,taste);
+        ReviewModel r3 = new ReviewModel(3,user,stamp,restaurant,menu,taste);
+
+        BaseListModel<ReviewModel> baselist = new BaseListModel<>();
+        List<ReviewModel> testList = new ArrayList<>();
+        testList.add(r1);
+        testList.add(r2);
+        testList.add(r3);
+
+        baselist.setData(testList);
+
+        return baselist;
+
     }
 
 }
