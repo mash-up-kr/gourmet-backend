9268b055c0120273cabdfbcd12e3071d05b421b8
bigstark <kkjkkdk@gmail.com>
2018-08-25 01:39:19 +0900

    Me / User / Review Controller Dummy data

--- api/src/main/java/com/kodachaya/gourmet/api/controller/me/MeController.java
+++ api/src/main/java/com/kodachaya/gourmet/api/controller/me/MeController.java
@@ -172,4 +172,51 @@ public class MeController {
         return response;
     }
 
+
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 500, message = "Failure")})
+    @RequestMapping(value = "/me/stamps", method = RequestMethod.GET)
+    public @ResponseBody BaseListModel<ReviewModel> getStamps(@RequestParam(value = "count", required = false, defaultValue = "15") Optional<Integer> optionalCount,
+                                                            @RequestParam(value = "cursor", required = false) Optional<Integer> optionalCursor) {
+
+        BaseListModel<ReviewModel> response = new BaseListModel<>();
+        response.setTotalCount(100);
+        response.setCount(15);
+        response.setCursor(16);
+
+        List<ReviewModel> reviews = new ArrayList<>(15);
+        for (int i = 0; i < 15; i++) {
+            ReviewModel review = new ReviewModel();
+            review.setId(0);
+            review.setStamp(i % 2 == 0 ? Stamp.LIKE : Stamp.DISLIKE);
+            review.setMenuTastes(Arrays.asList(Taste.BITTER, Taste.SALTY, Taste.SPICY, Taste.SWEET));
+
+            RestaurantModel restaurant = new RestaurantModel();
+            restaurant.setId(0);
+            restaurant.setRegisteredTime(LocalDateTime.now());
+            restaurant.setName("코다차야 강남점");
+            restaurant.setAddress("서울특별시 강남구 역삼동");
+            restaurant.setLatitude(37.5041302);
+            restaurant.setLongitude(127.025463);
+            review.setRestaurant(restaurant);
+
+            MenuModel menu = new MenuModel();
+            menu.setId(0);
+            menu.setPrice(12000);
+            menu.setName("김치볶음밥");
+            menu.setWishedAt(LocalDateTime.now());
+            menu.setWished(true);
+            menu.setRegisteredTime(LocalDateTime.now());
+            review.setMenu(menu);
+
+            reviews.add(review);
+        }
+        response.setData(reviews);
+
+        return response;
+    }
+
 }
